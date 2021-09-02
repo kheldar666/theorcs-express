87bb1e19b9d3c7ad056b4a36cdcc34028dad3aac
Martin Papy <martin.papy@gmail.com>
2021-09-02 11:41:50 +0700

    Small fix again

--- src/controllers/web/IndexController.ts
+++ src/controllers/web/IndexController.ts
@@ -1,13 +1,4 @@
-import {
-  $log,
-  Controller,
-  Get,
-  Locals,
-  Req,
-  Session,
-  UseBefore,
-  View,
-} from "@tsed/common";
+import { Controller, Get, Req, Session, UseBefore, View } from "@tsed/common";
 import { User } from "../../models/User";
 import { UserService } from "../../services/UserService";
 import { Inject } from "@tsed/di";
@@ -23,17 +14,20 @@ export class IndexController {
   private counter: number = 0;
 
   constructor(private userService: UserService) {}
+
   @Get("/")
   @View("index.ejs")
   async get(@Session() session: any, @Req() req: Req) {
     const newUser = new this.User({ name: "Martin Papy" });
     let setFlash = false;
     //const user = await this.userService.save(newUser);
+
     if (this.counter % 2) {
-      req.flash("error", "Test Error Flash Message");
-      req.flash("info", "Test Info Flash Message");
+      await req.flash("error", "Test Error Flash Message");
+      await req.flash("info", "Test Info Flash Message");
       setFlash = true;
     }
+
     this.counter++;
     return {
       user: newUser,
